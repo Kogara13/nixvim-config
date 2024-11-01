172f22992663a22e4693303278e0410f8c674a51
Kogara13 <kieranogara@gmail.com>
2024-11-01 00:04:44 -0400

    Added original lsp configs

--- config/lsp/default.nix
+++ config/lsp/default.nix
@@ -7,21 +7,35 @@
       servers = {
         bashls.enable = true;
         clangd.enable = true;
+        cmake.enable = true;
+        cssls.enable = true;
         elixirls.enable = true;
         gleam.enable = true;
         gopls.enable = true;
+        html.enable = true;
+        htmx.enable = true;
         kotlin_language_server.enable = true;
+        lua-ls = {
+            enable = true;
+            settings.telemetry.enable = false;
+        };
         nixd.enable = true;
         prolog_ls = {
           enable = true;
           package = pkgs.swiProlog;
         };
+        pylyzer.enable = true;
+        ruby-lsp.enable = true;
         ruff_lsp.enable = true;
         rust-analyzer = {
             enable = true;
             installCargo = true;
             installRustc = true;
         };
+        sqls.enable = true;
+        svelte.enable = true;
+        tailwindcss.enable = true;
+        tsserver.enable = true;
       };
       keymaps.lspBuf = {
         "gd" = "definition";
